42d6ad8482c1e0bc8a93ed211d6c8029a60787f7
ameera-ali <ameera.ali@education.nsw.gov.au>
2024-12-09 15:06:49 +1100

    Made the Database
    I followed the Database Tutorial document and made the Show_logs database, then some SQL queries to add into the table. I also made a server.js file to connect my front and backend, though it is not connecting properly yet.

--- myPWA/backend/database/querry.sql
+++ myPWA/backend/database/querry.sql
@@ -1,1 +1,16 @@
-SELECT * from show_logs;
+CREATE TABLE Shows(id INTEGER PRIMARY KEY AUTOINCREMENT,
+                  title TEXT NOT NULL,
+                  genre TEXT NOT NULL,
+                  rating INTEGER DEFAULT 0,
+                   review TEXT NOT NULL); 
+
+--INSERT INTO Shows(title, genre, rating, review) VALUES
+            ('The Rookie', 'Thriller', '4', 'Great police show'),
+            ('Carmen Sandiago', 'Action', '3', 'Interesting and fun'),
+            ('Gossip Girl', 'Comedy', '3', 'Really long and repetitive'),
+            ('YOU', 'Psychological Thriller', '4', 'kept me entertained the whole time, though the last season was not as great');
+
+--SELECT * FROM Shows;
+
+
+
